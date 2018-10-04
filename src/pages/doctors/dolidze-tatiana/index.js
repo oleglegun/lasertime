52697e0e1d10dbab4fa499aa5dfd28da40dce8d5
Oleg Legun <oleg.legun@gmail.com>
2018-10-04 18:11:10 +0300

    update doctors pages for gatsby v2

--- src/pages/doctors/dolidze-tatiana/index.js
+++ src/pages/doctors/dolidze-tatiana/index.js
@@ -1,53 +1,61 @@
 import React from 'react'
-import {graphql} from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 import Timeline, { TimelineItem } from '../../../components/Timeline'
 import Helmet from '../../../components/HelmetWrapper'
 import DoctorInfoPanel from '../../../components/DoctorInfoPanel'
 import certificates from './dolidze-tatiana-certificates.pdf'
+import Layout from '../../../components/Layout'
 
-function DolidzeTatianaDoctor({ data }) {
+function DolidzeTatianaDoctor() {
     return (
-        <div>
-            <Helmet
-                data={data}
-                title="Долидзе Татьяна Джумберовна"
-                description="Долидзе Татьяна Джумберовна - ассистент врача-косметолога в клинике &quot;Знак Качества&quot;"
+        <Layout>
+            <StaticQuery
+                query={pageQuery}
+                render={data => (
+                    <div>
+                        <Helmet
+                            data={data}
+                            title="Долидзе Татьяна Джумберовна"
+                            description='Долидзе Татьяна Джумберовна - ассистент врача-косметолога в клинике "Знак Качества"'
+                        />
+                        <DoctorInfoPanel
+                            name="Долидзе Татьяна Джумберовна"
+                            position="ассистент врача-косметолога"
+                            description="Владеет техниками косметического, пластического, испанского, точечного, скульптурирующего массажей."
+                            imageSizes={data.dolidzeTatianaGraybg.fluid}
+                        />
+                        <Timeline title="Обучение" certLink={certificates}>
+                            <TimelineItem
+                                date="2012"
+                                name="Люберецкий медицинский колледж"
+                                description='Диплом о среднем профессиональном образования по специальности "Сестринское дело"'
+                            />
+                            <TimelineItem
+                                date="2016"
+                                name="Российский университет дружбы народов (РУДН)"
+                                description='Диплом по специальности "Сестринское дело в косметологии"'
+                            />
+                            <TimelineItem
+                                date="2016"
+                                name='Научно-производственная фирма "Галатея"'
+                                description='Сертификат "Ультразвуковая и микротоковая терапия в косметологии"'
+                            />
+                            <TimelineItem
+                                date="2016"
+                                name="Первый профессиональный институт эстетики"
+                                description={[
+                                    'Сертификат "Объемное наращивание ресниц"',
+                                    'Сертификат "Мастер по наращиванию ресниц"',
+                                    'Сертификат "Скульптурный массаж лица (тибетская техника)"',
+                                    'Сертификат "Хиромассаж лица (испанская техника)"',
+                                    'Сертификат "Косметический (классический) массаж лица"',
+                                ]}
+                            />
+                        </Timeline>
+                    </div>
+                )}
             />
-            <DoctorInfoPanel
-                name="Долидзе Татьяна Джумберовна"
-                position="ассистент врача-косметолога"
-                description="Владеет техниками косметического, пластического, испанского, точечного, скульптурирующего массажей."
-                imageSizes={data.dolidzeTatianaGraybg.sizes}
-            />
-            <Timeline title="Обучение" certLink={certificates}>
-                <TimelineItem
-                    date="2012"
-                    name="Люберецкий медицинский колледж"
-                    description="Диплом о среднем профессиональном образования по специальности &quot;Сестринское дело&quot;"
-                />
-                <TimelineItem
-                    date="2016"
-                    name="Российский университет дружбы народов (РУДН)"
-                    description="Диплом по специальности &quot;Сестринское дело в косметологии&quot;"
-                />
-                <TimelineItem
-                    date="2016"
-                    name="Научно-производственная фирма &quot;Галатея&quot;"
-                    description="Сертификат &quot;Ультразвуковая и микротоковая терапия в косметологии&quot;"
-                />
-                <TimelineItem
-                    date="2016"
-                    name="Первый профессиональный институт эстетики"
-                    description={[
-                        'Сертификат "Объемное наращивание ресниц"',
-                        'Сертификат "Мастер по наращиванию ресниц"',
-                        'Сертификат "Скульптурный массаж лица (тибетская техника)"',
-                        'Сертификат "Хиромассаж лица (испанская техника)"',
-                        'Сертификат "Косметический (классический) массаж лица"',
-                    ]}
-                />
-            </Timeline>
-        </div>
+        </Layout>
     )
 }
 
@@ -57,10 +65,10 @@ export const pageQuery = graphql`
     query DolidzeTatianaDoctorQuery {
         ...Helmet
         dolidzeTatianaGraybg: imageSharp(
-            id: { regex: "/dolidze-tatiana-graybg/" }
+            fluid: { originalName: { regex: "/dolidze-tatiana-graybg/" } }
         ) {
-            sizes(maxWidth: 960, quality: 80) {
-                ...GatsbyImageSharpSizes_noBase64
+            fluid(maxWidth: 960, quality: 80) {
+                ...GatsbyImageSharpFluid_noBase64
             }
         }
     }
--- src/pages/doctors/portnova-ekaterina/index.js
+++ src/pages/doctors/portnova-ekaterina/index.js
@@ -1,157 +1,165 @@
 import React from 'react'
-import {graphql} from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 import Timeline, { TimelineItem } from '../../../components/Timeline'
 import Helmet from '../../../components/HelmetWrapper'
 import DoctorInfoPanel from '../../../components/DoctorInfoPanel'
 import certificates from './portnova-ekaterina-certificates.pdf'
+import Layout from '../../../components/Layout'
 
-function PortnovaEkaterinaDoctor({ data }) {
+function PortnovaEkaterinaDoctor( ) {
     return (
-        <div>
-            <Helmet
-                data={data}
-                title="Портнова Екатерина Вячеславовна"
-                description="Портнова Екатерина Вячеславовна - специалист по лазерным и световым технологиям в клинике &quot;Знак Качества&quot;"
+        <Layout>
+            <StaticQuery
+                query={pageQuery}
+                render={data => (
+                    <div>
+                        <Helmet
+                            data={data}
+                            title="Портнова Екатерина Вячеславовна"
+                            description='Портнова Екатерина Вячеславовна - специалист по лазерным и световым технологиям в клинике "Знак Качества"'
+                        />
+                        <DoctorInfoPanel
+                            name="Портнова Екатерина Вячеславовна"
+                            position="специалист по лазерным и световым технологиям"
+                            description='Ведущий специалист клиники "Знак Качества" по световым и лазерным технологиям. В основе ее работы лежат принципы сочетания комфорта и удобства для пациентов в процессе лечения. Стаж работы по специальности более 5 лет. Сертифицированный специалист по лазерным технологиям. Выполняет процедуры по улучшению состояния кожи с применением терапевтических методов.'
+                            imageSizes={data.portnovaEkaterinaGraybg.fluid}
+                        />
+                        <Timeline title="Обучение" certLink={certificates}>
+                            <TimelineItem
+                                date="2002 - 2006"
+                                name="МОМК №2"
+                                description={[
+                                    'Специальность "Сестринское дело" (с отличием)',
+                                    'Повышение квалификации, специализация "Физиотерапия"',
+                                    'Повышение квалификации, специализация "Косметология"',
+                                ]}
+                            />
+
+                            <TimelineItem
+                                date="2006"
+                                name='Компания "Syneron"'
+                                description="Сертификат по технологии Elos DS/SR"
+                            />
+
+                            <TimelineItem
+                                date="2008 - 2009"
+                                name="Эстетик Консалтинг"
+                                description={[
+                                    'Эстетические протоколы по коррекции фигуры',
+                                    'Мунальная техника эндермологического липомассажа',
+                                    'Сертификат по курсу обучения на аппарате Cellu MG Keymodule 1 "LPG Systems"',
+                                    'Сертификат по курсу обучения на аппарате Cellu MG Keymodule 2 "LPG Systems"',
+                                ]}
+                            />
+
+                            <TimelineItem
+                                date="2009"
+                                name="ГОУВПО СПБ ГМА им. М.И. Мечникова"
+                                description='Удостоверение "Мезотерапия в дерматологии и эстетической медицине"'
+                            />
+                            <TimelineItem
+                                date="2009"
+                                name="Компания DEKA"
+                                description={[
+                                    'Сертификат по лазерным технологиям на аппарате "Synchro Play"',
+                                    'Сертификат по лазерным технологиям на аппарате "Smart Xide DOT"',
+                                ]}
+                            />
+                            <TimelineItem
+                                date="2009"
+                                name='ООО "Вип Клиник"'
+                                description={[
+                                    'Сертификат по работе на аппарате VIP-LINE (коррекция фигуры)',
+                                    'Сертификат по работе на аппарате Revital RF (радиоволновой лифтинг)',
+                                    'Сертификат по работе на аппарате RAH-Medicell (ультразвуковая кавитация)',
+                                ]}
+                            />
+                            <TimelineItem
+                                date="2010"
+                                name="Учебно-методический центр MARTINEX"
+                                description="Диплом по работе с клеточной космецевтикой GERNETIC International"
+                            />
+                            <TimelineItem
+                                date="2011"
+                                name="Государственный научный центр лазерной медицины федерального медико-биологического агенства"
+                                description='Специализация "Лазерная дерматокосметология"'
+                            />
+                            <TimelineItem
+                                date="2011"
+                                name="МОМК №2"
+                                description='Повышение квалификации, специализация "Сестринская косметология"'
+                            />
+                            <TimelineItem
+                                date="2011"
+                                name="Компания Maruga"
+                                description="Сертификат по работе с пилингом Inno Peel"
+                            />
+
+                            <TimelineItem
+                                date="2012"
+                                name="Институт красоты Fijie Paris"
+                                description="Сертификат по работе с препаратом Meso-Wharton P199"
+                            />
+                            <TimelineItem
+                                date="2012"
+                                name='ООО "Астрея"'
+                                description="Применение профессиональных пилингов Mediderma/Sesderma/Sesvalia/Dietes"
+                            />
+                            <TimelineItem
+                                date="2012"
+                                name="Dermalogica"
+                                description="Сертификат по работе с космецевтикой Dermalogica"
+                            />
+                            <TimelineItem
+                                date="2012"
+                                name='Компания "Опти Мед"'
+                                description="Сертификат по работе с космецевтикой Hollyland"
+                            />
+                            <TimelineItem
+                                date=""
+                                name='ООО "Восточный экспресс"'
+                                description='Сертификат по работе с мезонитями "Тредлифтинг в противовозрастной медицине"'
+                            />
+                            <TimelineItem
+                                date="2013"
+                                name="Компания Mesoexpert"
+                                description="Сертификат по работе с мезонитями Reanane-V-line"
+                            />
+                            <TimelineItem
+                                date="2016"
+                                name="Дом Русской Косметики (ЧУ ДПО ДРК)"
+                                description={[
+                                    'Удостоверение "Современные инъекционные методы в эстетической медицине"',
+                                    'Сертификат по программе "Редермализация препаратом Hyalual"',
+                                    'Сертификат по контурной пластике',
+                                    'Сертификат по программе "Мезонити в программах коррекции возрастных изменений лица и тела"',
+                                    'Сертификат по работе "Ботулотоксин типа А"',
+                                    'Сертификат "Мезотерапия лица и тела"',
+                                    'Сертификат "Биоревитализация"',
+                                    'Сертификат "Склеротерапия"',
+                                    'Сертификат "Плазмолифтинг",',
+                                ]}
+                            />
+                            <TimelineItem
+                                date="2017"
+                                name=""
+                                description='Сертификат по программе "Лечение косметологических дефектов (пигментных сосудистых образований) и процедуры фотоэпиляции Quantum Lumenis"'
+                            />
+                            <TimelineItem
+                                date="2017"
+                                name='Компания "Опти Мед"'
+                                description='Сертификат по работе с космецевтикой "Holy Land"'
+                            />
+                            <TimelineItem
+                                date="2014 - 2017"
+                                name='Московский медицинский институт "Реавиз"'
+                                description="Обучение"
+                            />
+                        </Timeline>
+                    </div>
+                )}
             />
-            <DoctorInfoPanel
-                name="Портнова Екатерина Вячеславовна"
-                position="специалист по лазерным и световым технологиям"
-                description="Ведущий специалист клиники &quot;Знак Качества&quot; по световым и лазерным технологиям. В основе ее работы лежат принципы сочетания комфорта и удобства для пациентов в процессе лечения. Стаж работы по специальности более 5 лет. Сертифицированный специалист по лазерным технологиям. Выполняет процедуры по улучшению состояния кожи с применением терапевтических методов."
-                imageSizes={data.portnovaEkaterinaGraybg.sizes}
-            />
-            <Timeline title="Обучение" certLink={certificates}>
-                <TimelineItem
-                    date="2002 - 2006"
-                    name="МОМК №2"
-                    description={[
-                        'Специальность "Сестринское дело" (с отличием)',
-                        'Повышение квалификации, специализация "Физиотерапия"',
-                        'Повышение квалификации, специализация "Косметология"',
-                    ]}
-                />
-
-                <TimelineItem
-                    date="2006"
-                    name="Компания &quot;Syneron&quot;"
-                    description="Сертификат по технологии Elos DS/SR"
-                />
-
-                <TimelineItem
-                    date="2008 - 2009"
-                    name="Эстетик Консалтинг"
-                    description={[
-                        'Эстетические протоколы по коррекции фигуры',
-                        'Мунальная техника эндермологического липомассажа',
-                        'Сертификат по курсу обучения на аппарате Cellu MG Keymodule 1 "LPG Systems"',
-                        'Сертификат по курсу обучения на аппарате Cellu MG Keymodule 2 "LPG Systems"',
-                    ]}
-                />
-
-                <TimelineItem
-                    date="2009"
-                    name="ГОУВПО СПБ ГМА им. М.И. Мечникова"
-                    description="Удостоверение &quot;Мезотерапия в дерматологии и эстетической медицине&quot;"
-                />
-                <TimelineItem
-                    date="2009"
-                    name="Компания DEKA"
-                    description={[
-                        'Сертификат по лазерным технологиям на аппарате "Synchro Play"',
-                        'Сертификат по лазерным технологиям на аппарате "Smart Xide DOT"',
-                    ]}
-                />
-                <TimelineItem
-                    date="2009"
-                    name="ООО &quot;Вип Клиник&quot;"
-                    description={[
-                        'Сертификат по работе на аппарате VIP-LINE (коррекция фигуры)',
-                        'Сертификат по работе на аппарате Revital RF (радиоволновой лифтинг)',
-                        'Сертификат по работе на аппарате RAH-Medicell (ультразвуковая кавитация)',
-                    ]}
-                />
-                <TimelineItem
-                    date="2010"
-                    name="Учебно-методический центр MARTINEX"
-                    description="Диплом по работе с клеточной космецевтикой GERNETIC International"
-                />
-                <TimelineItem
-                    date="2011"
-                    name="Государственный научный центр лазерной медицины федерального медико-биологического агенства"
-                    description="Специализация &quot;Лазерная дерматокосметология&quot;"
-                />
-                <TimelineItem
-                    date="2011"
-                    name="МОМК №2"
-                    description="Повышение квалификации, специализация &quot;Сестринская косметология&quot;"
-                />
-                <TimelineItem
-                    date="2011"
-                    name="Компания Maruga"
-                    description="Сертификат по работе с пилингом Inno Peel"
-                />
-
-                <TimelineItem
-                    date="2012"
-                    name="Институт красоты Fijie Paris"
-                    description="Сертификат по работе с препаратом Meso-Wharton P199"
-                />
-                <TimelineItem
-                    date="2012"
-                    name="ООО &quot;Астрея&quot;"
-                    description="Применение профессиональных пилингов Mediderma/Sesderma/Sesvalia/Dietes"
-                />
-                <TimelineItem
-                    date="2012"
-                    name="Dermalogica"
-                    description="Сертификат по работе с космецевтикой Dermalogica"
-                />
-                <TimelineItem
-                    date="2012"
-                    name="Компания &quot;Опти Мед&quot;"
-                    description="Сертификат по работе с космецевтикой Hollyland"
-                />
-                <TimelineItem
-                    date=""
-                    name="ООО &quot;Восточный экспресс&quot;"
-                    description="Сертификат по работе с мезонитями &quot;Тредлифтинг в противовозрастной медицине&quot;"
-                />
-                <TimelineItem
-                    date="2013"
-                    name="Компания Mesoexpert"
-                    description="Сертификат по работе с мезонитями Reanane-V-line"
-                />
-                <TimelineItem
-                    date="2016"
-                    name="Дом Русской Косметики (ЧУ ДПО ДРК)"
-                    description={[
-                        'Удостоверение "Современные инъекционные методы в эстетической медицине"',
-                        'Сертификат по программе "Редермализация препаратом Hyalual"',
-                        'Сертификат по контурной пластике',
-                        'Сертификат по программе "Мезонити в программах коррекции возрастных изменений лица и тела"',
-                        'Сертификат по работе "Ботулотоксин типа А"',
-                        'Сертификат "Мезотерапия лица и тела"',
-                        'Сертификат "Биоревитализация"',
-                        'Сертификат "Склеротерапия"',
-                        'Сертификат "Плазмолифтинг",',
-                    ]}
-                />
-                <TimelineItem
-                    date="2017"
-                    name=""
-                    description="Сертификат по программе &quot;Лечение косметологических дефектов (пигментных сосудистых образований) и процедуры фотоэпиляции Quantum Lumenis&quot;"
-                />
-                <TimelineItem
-                    date="2017"
-                    name="Компания &quot;Опти Мед&quot;"
-                    description="Сертификат по работе с космецевтикой &quot;Holy Land&quot;"
-                />
-                <TimelineItem
-                    date="2014 - 2017"
-                    name="Московский медицинский институт &quot;Реавиз&quot;"
-                    description="Обучение"
-                />
-            </Timeline>
-        </div>
+        </Layout>
     )
 }
 
@@ -161,10 +169,10 @@ export const pageQuery = graphql`
     query PortnovaEkaterinaDoctorQuery {
         ...Helmet
         portnovaEkaterinaGraybg: imageSharp(
-            id: { regex: "/portnova-ekaterina-graybg/" }
+            fluid: { originalName: { regex: "/portnova-ekaterina-graybg/" } }
         ) {
-            sizes(maxWidth: 960, quality: 80) {
-                ...GatsbyImageSharpSizes_noBase64
+            fluid(maxWidth: 960, quality: 80) {
+                ...GatsbyImageSharpFluid_noBase64
             }
         }
     }
